5fe8b77f819a4018225889491e5558eaceae7d5e
Carlos <carlos.plana.garcia@estudiantat.upc.edu>
2022-04-25 10:44:04 +0200

    Added method element controller

---
+++ controllers/MethodElementController.php
@@ -0,0 +1,53 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/models/MethodElementModel.php';
+
+/**
+ * @OA\OpenApi(
+ *     @OA\Server(
+ *         url="localhost",
+ *         description="API server"
+ *     ),
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="SOPCOM",
+ *         description="SOPCOM REST API",
+ *     ),
+ * )
+ */
+class MethodElementController {
+
+    private $MethodElementModel;
+
+    function __construct()
+    {
+        $this->MethodElementModel = new MethodElement();
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/index.php/method-element/{methodElementId}", 
+     *     tags={"Method elements"},
+     *     summary="Find a method element by id",
+     *     description="Find a method element by id",
+     *     operationId="getMethodElement",
+     *     @OA\Parameter(
+     *         description="Id of the method element",
+     *         in="path",
+     *         name="methodElementId",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *         @OA\Examples(example="string", value="Chu-ReqEli-01", summary="A string value."),
+     *     ),
+     *     @OA\Response(response="200", description="OK"),
+     *     @OA\Response(response="404", description="Not found")
+     * )
+    */
+    public function getMethodElement($id) {
+        $result = $this->MethodElementModel->getMethodElementById($id);
+        header("Content-Type: application/json");
+        echo json_encode($result);
+    }
+}
+
+?>
