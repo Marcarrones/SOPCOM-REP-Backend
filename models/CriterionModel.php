1e44c68ba0e2b6d55d106d6655a01f7461069a60
Carlos-PG <carlos.plana.garcia@estudiantat.upc.edu>
2022-04-29 17:42:30 +0200

    Endpoint created

--- models/CriterionModel.php
+++ models/CriterionModel.php
@@ -3,9 +3,16 @@
         
         private $getAllCriterion = "SELECT c.id as criterionId, c.name as criterionName, v.id, v.name FROM criterion c, value v WHERE c.id = v.criterion;";
 
+        private $deleteCriterion = "DELETE FROM criterion WHERE id = ?;";
+
         public function getAllCriterions() {
             $statement = $this->conn->prepare($this->getAllCriterion);
             return $this->executeSelectQuery($statement);
+        }
+
+        public function deleteCriterion() {
+            $statement = $this->conn->prepare($this->deleteCriterion);
+            return $this->executeDeleteQuery($statement);
         }
     }
 
