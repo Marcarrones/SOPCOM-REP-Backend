4245c71aa8a139b0268f2962c111c677a2bfbdc3
Carlos-PG <carlos.plana.garcia@estudiantat.upc.edu>
2022-04-29 17:14:38 +0200

    Fixed query

--- models/CriterionModel.php
+++ models/CriterionModel.php
@@ -1,7 +1,7 @@
 <?php
     class Criterion extends Model{
         
-        private $getAllCriterion = "SELECT c.id, c.name, v.id, v.name FROM criterion c, value v WHERE c.id = v.criterion;";
+        private $getAllCriterion = "SELECT c.id as criterionId, c.name as criterionName, v.id, v.name FROM criterion c, value v WHERE c.id = v.criterion;";
 
         public function getAllCriterions() {
             $statement = $this->conn->prepare($this->getAllCriterion);
